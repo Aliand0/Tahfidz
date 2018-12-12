8b6c6e4ca104c78bc3c99310ac1f1252f153bfea
unknown <ajifajar23@gmail.com>
2018-12-13 04:59:59 +0700

    create kelas new & create hafalan -

--- resources/views/kelas/create.blade.php
+++ resources/views/kelas/create.blade.php
@@ -7,35 +7,13 @@ $(document).ready(function() {
 });
 
 </script>
-
-<script type="text/javascript">
-        function readURL() {
-            var input = this;
-            if (input.files && input.files[0]) {
-                var reader = new FileReader();
-                reader.onload = function (e) {
-                    $(input).prev().attr('src', e.target.result);
-                }
-                reader.readAsDataURL(input.files[0]);
-            }
-        }
-
-        $(function () {
-            $(".uploads").change(readURL)
-            $("#f").submit(function(){
-                // do ajax submit or just classic form submit
-              //  alert("fake subminting")
-                return false
-            })
-        })
-        </script>
 @stop
 
 @extends('layouts.app')
 
 @section('content')
 
-<form method="POST" action="{{ route('buku.store') }}" enctype="multipart/form-data">
+<form method="POST" action="{{ route('anggota.store') }}" enctype="multipart/form-data">
     {{ csrf_field() }}
 <div class="row">
             <div class="col-md-12 d-flex align-items-stretch grid-margin">
@@ -43,58 +21,37 @@ $(document).ready(function() {
                 <div class="col-12">
                   <div class="card">
                     <div class="card-body">
-<<<<<<< HEAD
-                      <h4 class="card-title">Tambah Kelas Baru</h4>
-                      
-                        <div class="form-group{{ $errors->has('judul') ? ' has-error' : '' }}">
-                            <label for="judul" class="col-md-4 control-label">Kelas</label>
-=======
-                      <h4 class="card-title">Tambah Kelas baru</h4>
-                      
-                        <div class="form-group{{ $errors->has('judul') ? ' has-error' : '' }}">
-                            <label for="judul" class="col-md-4 control-label">Nama Kelas</label>
->>>>>>> 0e661f755d2a6b68438cab3d3d181e99a3555547
-                            <div class="col-md-6">
-                                <input id="judul" type="text" class="form-control" name="judul" value="{{ old('judul') }}" required>
-                                @if ($errors->has('judul'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('judul') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-                        
-                        <div class="form-group{{ $errors->has('tahun_terbit') ? ' has-error' : '' }}">
-                            <label for="tahun_terbit" class="col-md-4 control-label">Tahun Pelajaran</label>
-                            <div class="col-md-6">
-                                <input id="tahun_terbit" type="number" maxlength="4" class="form-control" name="tahun_terbit" value="{{ old('tahun_terbit') }}" required>
-                                @if ($errors->has('tahun_terbit'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('tahun_terbit') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-                        <div class="form-group{{ $errors->has('jumlah_buku') ? ' has-error' : '' }}">
-                            <label for="jumlah_buku" class="col-md-4 control-label">Jumlah Siswa</label>
-                            <div class="col-md-6">
-                                <input id="jumlah_buku" type="number" maxlength="4" class="form-control" name="jumlah_buku" value="{{ old('jumlah_buku') }}" required>
-                                @if ($errors->has('jumlah_buku'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('jumlah_buku') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-                
+                      <h4 class="card-title">Tambah Kelas</h4>
 
+                        <div class="form-group{{ $errors->has('kelas') ? ' has-error' : '' }}">
+                            <label for="kelas" class="col-md-4 control-label">kelas</label>
+                            <div class="col-md-6">
+                                <input id="kelas" type="text" class="form-control" name="kelas" value="{{ old('kelas') }}" required>
+                                @if ($errors->has('kelas'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('kelas') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="form-group{{ $errors->has('tahun') ? ' has-error' : '' }}">
+                            <label for="tahun" class="col-md-4 control-label">tahun</label>
+                            <div class="col-md-6">
+                                <input id="tahun" type="number" class="form-control" name="tahun" value="{{ old('tahun') }}" maxlength="8" required>
+                                @if ($errors->has('tahun'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('tahun') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
                         <button type="submit" class="btn btn-primary" id="submit">
                                     Submit
                         </button>
                         <button type="reset" class="btn btn-danger">
                                     Reset
                         </button>
-                        <a href="{{route('buku.index')}}" class="btn btn-light pull-right">Back</a>
+                        <a href="{{route('anggota.index')}}" class="btn btn-light pull-right">Back</a>
                     </div>
                   </div>
                 </div>
